f388ac7aa1553920321d47955976b5569c7bd663
Scott Gress <scottmgress@gmail.com>
2015-03-14 15:08:45 -0700

    Get the best error display possible for 500 pages

--- templates/api/responses/serverError.js
+++ templates/api/responses/serverError.js
@@ -19,6 +19,19 @@ module.exports = function serverError (data, options) {
   var res = this.res;
   var sails = req._sails;
 
+  // If the data is an Error object, leave it as is; it will be
+  // inspected and stringified automagically.  If not, we'll attempt
+  // to stringify it, and failing that, just clear it out (better than
+  // displaying [Object object])
+  if (!(data instanceof Error) && _.isObject(data)) {
+    try {
+      data = JSON.stringify(data);
+    }
+    catch(e) {
+      data = undefined;
+    }
+  }
+
   // Set status code
   res.status(500);
 
